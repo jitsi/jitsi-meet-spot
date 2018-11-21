2c9e2ae77d4f7c5f2e4f9a5897424019609a484f
Leonard Kim <leonard.kim@8x8.com>
2018-11-21 13:35:18 -0800

    feat(tests): script to run all tests on ci

---
+++ scripts/ci.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+pid=""
+
+npm install
+npm run lint
+npm run test
+
+if [ -z "$TEST_SERVER_URL" ]; then
+    npm run build-prod
+
+    port=TEST_PORT
+    export TEST_SERVER_URL="http://localhost:$TEST_PORT"
+
+    python -m SimpleHTTPServer $TEST_PORT &
+    pid=$!
+
+    sleep 1
+
+    ps aux | grep "$pid" | grep -v "grep"
+    [ $? -eq 0 ] || exit $?;
+fi
+
+npm run ui-tests
+
+kill $pid
--- ui-tests/specs/calender.spec.js
+++ ui-tests/specs/calender.spec.js
@@ -1,6 +1,6 @@
 describe('calendar', () => {
     it('loads', async () => {
-        await browser.url(process.env.URL);
+        await browser.url(process.env.TEST_SERVER_URL);
 
         expect(await browser.getTitle()).toBe('Spot');
     });
