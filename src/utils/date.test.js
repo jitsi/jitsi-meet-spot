8d3977b73afe458e267c89c30bc9d588e4a8dfc1
Leonard Kim <leonard.kim@8x8.com>
2018-11-20 17:43:29 -0800

    squash: fix date test by setting explicit time

--- src/utils/date.test.js
+++ src/utils/date.test.js
@@ -2,16 +2,31 @@ import date from './date';
 
 describe('formatToTime', () => {
     test('returns the time in hh:mm format', () => {
-        expect(date.formatToTime(1542683873254)).toBe('07:17');
+        const testDate = new Date();
+        testDate.setMonth(11);
+        testDate.setDate(20);
+        testDate.setHours(19);
+        testDate.setMinutes(17);
+
+        expect(date.formatToTime(testDate)).toBe('07:17');
     });
 });
 
 describe('getEndOfDate', () => {
     test('returns a date object set to the end of the passed-in day', () => {
-        const testDate = new Date(1542683873254);
-        const calculatedDate = date.getEndOfDate(testDate);
-        const expectedDate = new Date(1542700799999);
+        const testDate = new Date();
+        testDate.setMonth(11);
+        testDate.setDate(20);
+        testDate.setHours(19);
+        testDate.setMinutes(17);
+        testDate.setFullYear(2018);
 
-        expect(calculatedDate.getTime()).toBe(expectedDate.getTime());
+        const calculatedDate = date.getEndOfDate(testDate);
+
+        expect(calculatedDate.getFullYear()).toBe(2018);
+        expect(calculatedDate.getMonth()).toBe(11);
+        expect(calculatedDate.getDate()).toBe(20);
+        expect(calculatedDate.getHours()).toBe(23);
+        expect(calculatedDate.getMinutes()).toBe(59);
     });
 });
