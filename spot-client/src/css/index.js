69bf67b8498cd1e159dcb5bbe0338ff2297e71eb
Leonard Kim <leonard.kim@8x8.com>
2019-02-07 10:09:57 -0800

    ref(css): scss files only

--- spot-client/src/css/index.js
+++ spot-client/src/css/index.js
@@ -1,10 +1,10 @@
 import './media-queries.scss';
-import './variables.css';
+import './variables.scss';
 
-import './reset.css';
+import './reset.scss';
 import './app.scss';
 
 import './spot.scss';
-import './views.css';
+import './views.scss';
 import './features.scss';
 import './remote.scss';
