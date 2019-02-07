c23e94fbf840fdf4e24a551dff0bfd1c65968690
Leonard Kim <leonard.kim@8x8.com>
2019-02-07 13:10:59 -0800

    ref(css): rename ambigious spot.scss to home-view

--- spot-client/src/css/index.js
+++ spot-client/src/css/index.js
@@ -4,9 +4,9 @@ import './reset.scss';
 import './app.scss';
 
 import './clock.scss';
+import './home-view.scss';
 import './meeting.scss';
 import './nav.scss';
 
-import './spot.scss';
 import './temp.scss';
 import './remote.scss';
