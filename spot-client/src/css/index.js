3880697466f54b1fc834ecfec07734e2f24e2226
Leonard Kim <leonard.kim@8x8.com>
2019-02-07 13:35:38 -0800

    ref(css): move resets into one file

--- spot-client/src/css/index.js
+++ spot-client/src/css/index.js
@@ -1,13 +1,12 @@
 import './variables.scss';
 
 import './reset.scss';
-import './app.scss';
 
 import './clock.scss';
 import './home-view.scss';
 import './meeting.scss';
 import './meeting-name-entry.scss';
-import './remote-control-views.scss';
 import './nav.scss';
+import './remote-control-views.scss';
 
 import './temp.scss';
