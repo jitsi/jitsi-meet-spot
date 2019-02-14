b383bb7156b21da51ed9f5d9e6aa57b6d3556dd2
virtuacoplenny <virtuacoplenny@gmail.com>
2019-02-14 12:53:33 -0800

    feat(feedback): show text box for 3 or less rating (#94)

--- spot-client/src/spot-remote/ui/components/remote-control-menu/feedback-form.js
+++ spot-client/src/spot-remote/ui/components/remote-control-menu/feedback-form.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import { logger } from 'common/logger';
+
 /**
  * A React Component for inputting and submitting post-call feedback by leaving
  * a rating and a
@@ -22,9 +24,13 @@ export default class FeedbackForm extends React.Component {
         super(props);
 
         this.state = {
+            hasSubmittedStars: false,
+            message: '',
+            requestMoreInfo: false,
             score: -1
         };
 
+        this._onMessageChange = this._onMessageChange.bind(this);
         this._onRatingChange = this._onRatingChange.bind(this);
         this._onSubmit = this._onSubmit.bind(this);
     }
@@ -41,15 +47,96 @@ export default class FeedbackForm extends React.Component {
                 className = 'remote-feedback'
                 onSubmit = { this._onSubmit }>
                 <div className = 'cta'>Rate your experience</div>
-                <div className = 'stars'>
-                    { this._renderStars() }
-                </div>
+                { this.state.requestMoreInfo
+                    ? this._renderInfoRequest()
+                    : this._renderStars() }
                 <button
                     className = 'submit-button'
                     type = 'submit'>
                     Send
                 </button>
             </form>
+        );
+    }
+
+    /**
+     * Callback invoked when the entered additional feedback details is updated.
+     *
+     * @param {ChangeEvent} e - The DOM event from the message field being
+     * updated.
+     * @private
+     * @returns {void}
+     */
+    _onMessageChange(e) {
+        this.setState({
+            message: e.target.value
+        });
+    }
+
+    /**
+     * Updates the known number of stars selected for rating the meeting
+     * experience.
+     *
+     * @param {int} score - The star value that has been selected.
+     * @private
+     * @returns {void}
+     */
+    _onRatingChange(score) {
+        this.setState({ score });
+    }
+
+    /**
+     * Sends the entered feedback to the Spot instance so it can be submitted.
+     *
+     * @param {Event} event - The submit event passed through by the form.
+     * @private
+     * @returns {void}
+     */
+    _onSubmit(event) {
+        event.preventDefault();
+
+        if (!this.state.hasSubmittedStars
+            && this.state.score <= 3
+            && this.state.score !== -1) {
+            logger.log('Feedback requesting additional information');
+
+            this.setState({
+                hasSubmittedStars: true,
+                requestMoreInfo: true
+            });
+
+            return;
+        }
+
+        logger.log('Feedback submitting');
+
+        this.props.remoteControlService.submitFeedback({
+            message: this.state.message,
+            score: this.state.score
+        });
+    }
+
+    /**
+     * Renders a React Element for entering additional details about the
+     * submitted rating.
+     *
+     * @private
+     * @returns {ReactElement}
+     */
+    _renderInfoRequest() {
+        return (
+            <div>
+                <div className = 'message-help'>
+                    Please tell us more about it.
+                </div>
+                <textarea
+                    autoFocus = { true }
+                    className = 'remote-message'
+                    onChange = { this._onMessageChange }
+                    rows = { 4 }
+                    spellCheck = { false }
+                    value = { this.state.message } />
+            </div>
         );
     }
 
@@ -82,31 +169,10 @@ export default class FeedbackForm extends React.Component {
             );
         }
 
-        return stars;
-    }
-
-    /**
-     * Updates the known number of stars selected for rating the meeting
-     * experience.
-     *
-     * @param {int} score - The star value that has been selected.
-     * @private
-     * @returns {void}
-     */
-    _onRatingChange(score) {
-        this.setState({ score });
-    }
-
-    /**
-     * Sends the entered feedback to the Spot instance so it can be submitted.
-     *
-     * @param {Event} event - The submit event passed through by the form.
-     * @private
-     * @returns {void}
-     */
-    _onSubmit(event) {
-        event.preventDefault();
-
-        this.props.remoteControlService.submitFeedback(this.state);
+        return (
+            <div className = 'stars'>
+                { stars }
+            </div>
+        );
     }
 }
