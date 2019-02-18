5156b97c6da1dce7925463db7f41e793204e754c
Leonard Kim <leonard.kim@8x8.com>
2019-02-17 19:58:24 -0800

    fix(controller): import acorn to fix lint not running

--- in-room-controller/src/remote-control.js
+++ in-room-controller/src/remote-control.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import {
     StyleSheet,
-    View,
+    View
 } from 'react-native';
 import { WebView } from 'react-native-webview';
 
