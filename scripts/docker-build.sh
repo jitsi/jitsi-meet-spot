973b03aea062cb18e2ef1ed3837e7b85cbb407b3
Aaron van Meerten <aaron.van.meerten@gmail.com>
2019-02-27 15:43:28 -0600

    docker build now also runs npm build

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -8,6 +8,8 @@ fi
 DOCKER_REPO=103425057857.dkr.ecr.us-west-2.amazonaws.com/jitsi/spot-client
 
 cd spot-client
+npm install
+npm run build:prod
 $(aws ecr get-login --no-include-email)
 docker build -t $DOCKER_REPO:$TAG .
 docker push $DOCKER_REPO:$TAG
