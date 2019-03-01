8826b020658f9226915d3d799179ab8a4967b2dc
paweldomas <pawel.domas@jitsi.org>
2019-03-01 13:04:43 -0600

    ref: extract a ScreenshareButton to a separate file

--- spot-client/src/spot-remote/ui/components/remote-control-menu/remote-control-menu.js
+++ spot-client/src/spot-remote/ui/components/remote-control-menu/remote-control-menu.js
@@ -1,11 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { JitsiMeetJSProvider } from 'common/vendor';
 import { NavButton, NavContainer } from '../nav';
 
-import Popover from './popover';
-import ScreensharePicker from './screenshare-picker';
+import ScreenshareButton from './screenshare-button';
 
 /**
  * Displays buttons used for remotely controlling a Spot instance.
@@ -31,20 +29,9 @@ export default class RemoteControlMenu extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            showScreensharePicker: false
-        };
-
         this._onHangUp = this._onHangUp.bind(this);
-        this._onHideScreensharePicker
-            = this._onHideScreensharePicker.bind(this);
-        this._onToggleScreensharePicker
-            = this._onToggleScreensharePicker.bind(this);
         this._onToggleAudioMute = this._onToggleAudioMute.bind(this);
-        this._onToggleScreensharing = this._onToggleScreensharing.bind(this);
         this._onToggleVideoMute = this._onToggleVideoMute.bind(this);
-        this._onToggleWirelessScreensharing
-            = this._onToggleWirelessScreensharing.bind(this);
     }
 
     /**
@@ -68,7 +55,12 @@ export default class RemoteControlMenu extends React.Component {
                     label
                         = { videoMuted ? 'Unmute Video' : 'Mute Video' }
                     onClick = { this._onToggleVideoMute } />
-                { this._renderScreensharingButton() }
+                <ScreenshareButton
+                    isWirelessScreenshareConnectionActive
+                        = { this.props.isWirelessScreenshareConnectionActive }
+                    remoteControlService = { this.props.remoteControlService }
+                    screensharing = { this.props.screensharing }
+                    screensharingEnabled = { this.props.screensharingEnabled } />
                 <NavButton
                     className = 'hangup'
                     iconName = 'call_end'
@@ -89,18 +81,6 @@ export default class RemoteControlMenu extends React.Component {
     }
 
     /**
-     * Hides the screenshare picker.
-     *
-     * @private
-     * @returns {void}
-     */
-    _onHideScreensharePicker() {
-        this.setState({
-            showScreensharePicker: false
-        });
-    }
-
-    /**
      * Changes the current local audio mute state.
      *
      * @private
@@ -111,19 +91,6 @@ export default class RemoteControlMenu extends React.Component {
     }
 
     /**
-     * Starts or stops local screensharing.
-     *
-     * @private
-     * @returns {void}
-     */
-    _onToggleScreensharing() {
-        this._onHideScreensharePicker();
-
-        this.props.remoteControlService.setScreensharing(
-            !this.props.screensharing);
-    }
-
-    /**
      * Changes the current local video mute state.
      *
      * @private
@@ -131,113 +98,5 @@ export default class RemoteControlMenu extends React.Component {
      */
     _onToggleVideoMute() {
         this.props.remoteControlService.setVideoMute(!this.props.videoMuted);
-    }
-
-    /**
-     * Changes whether the screenshare picker is displayed or not.
-     *
-     * @private
-     * @returns {void}
-     */
-    _onToggleScreensharePicker() {
-        this.setState({
-            showScreensharePicker: !this.state.showScreensharePicker
-        });
-    }
-
-    /**
-     * Changes the current wireless screensharing state.
-     *
-     * @private
-     * @returns {void}
-     */
-    _onToggleWirelessScreensharing() {
-        this._onHideScreensharePicker();
-
-        const {
-            isWirelessScreenshareConnectionActive,
-            remoteControlService,
-            screensharing
-        } = this.props;
-
-        // No-op if a proxy connection to set up screensharing is in progress.
-        if (isWirelessScreenshareConnectionActive && !screensharing) {
-            return;
-        }
-
-        remoteControlService.setWirelessScreensharing(!screensharing);
-    }
-
-    /**
-     * Returns the screenshare button that should be displayed, if any.
-     *
-     * @private
-     * @returns {ReactElement}
-     */
-    _renderScreensharingButton() {
-        const {
-            screensharing,
-            screensharingEnabled
-        } = this.props;
-        const canWirelessScreenshare = JitsiMeetJSProvider.isWirelessScreenshareSupported();
-
-        // If neither screensharing mode is allowed then show nothing.
-        if (!screensharingEnabled && !canWirelessScreenshare) {
-            return null;
-        }
-
-        // If screensharing is active and at least one mode is allowed then show
-        // a stop button.
-        if (screensharing) {
-            return (
-                <NavButton
-                    iconName = 'stop_screen_share'
-                    label = 'Stop Sharing'
-                    onClick = { this._onToggleScreensharing } />
-            );
-        }
-
-        // If only wired screensharing is enabled show only its button.
-        if (screensharingEnabled && !canWirelessScreenshare) {
-            return (
-                <NavButton
-                    className = 'screenshare'
-                    iconName = 'screen_share'
-                    label = 'Share Content'
-                    onClick = { this._onToggleScreensharing } />
-            );
-        }
-
-        // If only wireless screensharing is enabled show only its button.
-        // if (true) {
-        if (canWirelessScreenshare && !screensharingEnabled) {
-            return (
-                <NavButton
-                    className = 'screenshare'
-                    iconName = 'screen_share'
-                    label = 'Share Content'
-                    onClick = { this._onToggleWirelessScreensharing } />
-            );
-        }
-
-        // If both screensharings are allowed but neither is active then show
-        // a screenshare picker.
-        return (
-            <Popover
-                onOutsideClick = { this._onHideScreensharePicker }
-                popoverContent = {
-                    <ScreensharePicker
-                        onStartWiredScreenshare
-                            = { this._onToggleScreensharing }
-                        onStartWirelessScreenshare
-                            = { this._onToggleWirelessScreensharing } />
-                }
-                showPopover = { this.state.showScreensharePicker }>
-                <NavButton
-                    iconName = 'screen_share'
-                    label = 'Share Content'
-                    onClick = { this._onToggleScreensharePicker } />
-            </Popover>
-        );
     }
 }
---
+++ spot-client/src/spot-remote/ui/components/remote-control-menu/screenshare-button.js
@@ -0,0 +1,173 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { JitsiMeetJSProvider } from 'common/vendor';
+import { NavButton } from '../nav';
+import Popover from './popover';
+import ScreensharePicker from './screenshare-picker';
+
+
+/**
+ * Displays a screensharing button. It toggles the screensharing on and off and allows to select
+ * between wireless and wired.
+ *
+ * @extends React.Component
+ */
+export default class ScreenshareButton extends React.Component {
+    static propTypes = {
+        isWirelessScreenshareConnectionActive: PropTypes.bool,
+        remoteControlService: PropTypes.object,
+        screensharing: PropTypes.bool,
+        screensharingEnabled: PropTypes.bool
+    };
+
+    /**
+     * Initializes a new {@code RemoteControlMenu} instance.
+     *
+     * @param {Object} props - The read-only properties with which the new
+     * instance is to be initialized.
+     */
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showScreensharePicker: false
+        };
+
+        this._onHideScreensharePicker = this._onHideScreensharePicker.bind(this);
+        this._onToggleScreensharePicker = this._onToggleScreensharePicker.bind(this);
+        this._onToggleScreensharing = this._onToggleScreensharing.bind(this);
+        this._onToggleWirelessScreensharing = this._onToggleWirelessScreensharing.bind(this);
+    }
+
+    /**
+     * Implements React's {@link Component#render()}.
+     *
+     * @inheritdoc
+     */
+    render() {
+        const {
+            screensharing,
+            screensharingEnabled
+        } = this.props;
+        const canWirelessScreenshare = JitsiMeetJSProvider.isWirelessScreenshareSupported();
+
+        // If neither screensharing mode is allowed then show nothing.
+        if (!screensharingEnabled && !canWirelessScreenshare) {
+            return null;
+        }
+
+        // If screensharing is active and at least one mode is allowed then show
+        // a stop button.
+        if (screensharing) {
+            return (
+                <NavButton
+                    iconName = 'stop_screen_share'
+                    label = 'Stop Sharing'
+                    onClick = { this._onToggleScreensharing } />
+            );
+        }
+
+        // If only wired screensharing is enabled show only its button.
+        if (screensharingEnabled && !canWirelessScreenshare) {
+            return (
+                <NavButton
+                    className = 'screenshare'
+                    iconName = 'screen_share'
+                    label = 'Share Content'
+                    onClick = { this._onToggleScreensharing } />
+            );
+        }
+
+        // If only wireless screensharing is enabled show only its button.
+        // if (true) {
+        if (canWirelessScreenshare && !screensharingEnabled) {
+            return (
+                <NavButton
+                    className = 'screenshare'
+                    iconName = 'screen_share'
+                    label = 'Share Content'
+                    onClick = { this._onToggleWirelessScreensharing } />
+            );
+        }
+
+        // If both screensharings are allowed but neither is active then show
+        // a screenshare picker.
+        return (
+            <Popover
+                onOutsideClick = { this._onHideScreensharePicker }
+                popoverContent = {
+                    <ScreensharePicker
+                        onStartWiredScreenshare
+                            = { this._onToggleScreensharing }
+                        onStartWirelessScreenshare
+                            = { this._onToggleWirelessScreensharing } />
+                }
+                showPopover = { this.state.showScreensharePicker }>
+                <NavButton
+                    iconName = 'screen_share'
+                    label = 'Share Content'
+                    onClick = { this._onToggleScreensharePicker } />
+            </Popover>
+        );
+    }
+
+    /**
+     * Hides the screenshare picker.
+     *
+     * @private
+     * @returns {void}
+     */
+    _onHideScreensharePicker() {
+        this.setState({
+            showScreensharePicker: false
+        });
+    }
+
+    /**
+     * Starts or stops local screensharing.
+     *
+     * @private
+     * @returns {void}
+     */
+    _onToggleScreensharing() {
+        this._onHideScreensharePicker();
+
+        this.props.remoteControlService.setScreensharing(!this.props.screensharing);
+    }
+
+    /**
+     * Changes whether the screenshare picker is displayed or not.
+     *
+     * @private
+     * @returns {void}
+     */
+    _onToggleScreensharePicker() {
+        this.setState({
+            showScreensharePicker: !this.state.showScreensharePicker
+        });
+    }
+
+    /**
+     * Changes the current wireless screensharing state.
+     *
+     * @private
+     * @returns {void}
+     */
+    _onToggleWirelessScreensharing() {
+        this._onHideScreensharePicker();
+
+        const {
+            isWirelessScreenshareConnectionActive,
+            remoteControlService,
+            screensharing
+        } = this.props;
+
+        // No-op if a proxy connection to set up screensharing is in progress.
+        if (isWirelessScreenshareConnectionActive && !screensharing) {
+            return;
+        }
+
+        remoteControlService.setWirelessScreensharing(!screensharing);
+    }
+}
