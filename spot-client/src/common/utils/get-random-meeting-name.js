1b4e6c989f38c19623a8f1d37d8062df0ab58152
virtuacoplenny <virtuacoplenny@gmail.com>
2019-03-18 17:58:15 -0700

    fix(meet-now): use jitsi-meet's random name logic (#162)
    * fix(meet-now): use jitsi-meet's random name logic
    
    * update sha for js-utils merge

--- spot-client/src/common/utils/get-random-meeting-name.js
+++ spot-client/src/common/utils/get-random-meeting-name.js
@@ -1,49 +1,10 @@
-const articles = [
-    'The',
-    'My',
-    'Our',
-    'A' // Special case handling needed for proper grammar.
-];
-const articlesCount = articles.length;
-const vowels = new Set([ 'A', 'E', 'I', 'O', 'U' ]);
-const prefixes = [
-    'Adhoc',
-    'Brainstorming',
-    'Ideation',
-    'Impromptu',
-    'Planning',
-    'Spontaneous',
-    'Unplanned'
-];
-const prefixCount = prefixes.length;
-const suffixes = [
-    'Call',
-    'Conversation',
-    'Dialogue',
-    'Discussion',
-    'Exchange',
-    'Huddle',
-    'Meeting',
-    'Standup',
-    'Talk'
-];
-const suffixCount = suffixes.length;
+import { generateRoomWithoutSeparator } from 'js-utils/random';
 
 /**
- * Generates a random meeting name by picking a random value from the constants
- * above.
+ * Generates a new room name.
  *
- * @returns {string}
+ * @returns {string} A newly-generated room name.
  */
 export function getRandomMeetingName() {
-    const randomPrefix = prefixes[Math.floor(Math.random() * prefixCount)];
-    const randomSuffix = suffixes[Math.floor(Math.random() * suffixCount)];
-
-    let article = articles[Math.floor(Math.random() * articlesCount)];
-
-    if (article === 'A' && vowels.has(randomPrefix.charAt(0))) {
-        article = 'An';
-    }
-
-    return `${article}${randomPrefix}${randomSuffix}`;
+    return generateRoomWithoutSeparator();
 }
