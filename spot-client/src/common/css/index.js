f2bf99a1326bec5e05a195513055011f6d23564e
Leonard Kim <leonard.kim@8x8.com>
2019-03-23 15:05:39 -0700

    ref(css): break out spot-remote layout files

--- spot-client/src/common/css/index.js
+++ spot-client/src/common/css/index.js
@@ -3,6 +3,13 @@ import './variables.scss';
 import './reset.scss';
 import './material-icons/icon-font.scss';
 
+import './nav/nav.scss';
+import './nav/nav-button.scss';
+import './page-layouts/feedback-view.scss';
+import './page-layouts/in-call-view.scss';
+import './page-layouts/join-code-view.scss';
+import './page-layouts/waiting-view.scss';
+
 import './admin-toolbar.scss';
 import './clock.scss';
 import './code-entry.scss';
@@ -10,13 +17,8 @@ import './desktop-picker.scss';
 import './dial-pad.scss';
 import './home-view.scss';
 import './idle-cursor.scss';
-import './join-code-view.scss';
 import './meeting.scss';
 import './meeting-name-entry.scss';
 import './modal.scss';
-import './nav/nav.scss';
-import './nav/nav-button.scss';
-import './remote-control-views.scss';
 import './screenshare.scss';
-
 import './temp.scss';
