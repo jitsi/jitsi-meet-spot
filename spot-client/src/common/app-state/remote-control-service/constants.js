f04d5d8dbd15cfcce5a20847ef7687b6091a32d0
Leonard Kim <leonard.kim@8x8.com>
2019-03-26 19:54:43 -0700

    feat(nav): pending states for audio and video mute

---
+++ spot-client/src/common/app-state/remote-control-service/constants.js
@@ -0,0 +1,10 @@
+export const requestStates = {
+    DONE: 'DONE',
+    ERROR: 'ERROR',
+    PENDING: 'PENDING'
+};
+
+export const requestTypes = {
+    AUDIO_MUTE: 'audioMute',
+    VIDEO_MUTE: 'videoMute'
+};
