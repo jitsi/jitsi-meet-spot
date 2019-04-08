863bc850d5946c90172d52734fe23fdb00e8beff
Leonard Kim <leonard.kim@8x8.com>
2019-04-08 14:11:37 -0700

    fix lint

--- spot-client/src/spot-remote/ui/components/nav/nav-button.js
+++ spot-client/src/spot-remote/ui/components/nav/nav-button.js
@@ -68,7 +68,7 @@ export default class NavButton extends React.Component {
         }
 
         if (className) {
-            rootClassName +=  ` ${className}`;
+            rootClassName += ` ${className}`;
         }
 
         return (
