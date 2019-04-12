d949ac4600e097e85c8bc48f2430edd42f854afd
virtuacoplenny <virtuacoplenny@gmail.com>
2019-04-12 08:17:27 -0700

    ref(remote): remove old, unused remote control menu (#222)

--- spot-client/src/spot-remote/ui/components/remote-control-menu/index.js
+++ spot-client/src/spot-remote/ui/components/remote-control-menu/index.js
@@ -1,3 +1,2 @@
 export { default as FeedbackForm } from './feedback-form';
-export { default as RemoteControlMenu } from './remote-control-menu';
 export { default as ScreensharePicker } from './screenshare-picker';
