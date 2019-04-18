4eb20c507ec72b66fae79df9eff5f17a10a89be2
paweldomas <pawel.domas@jitsi.org>
2019-04-18 10:20:02 -0500

    feat(analytics): report audio and video mute events from Spot Remote

--- spot-client/src/spot-remote/analytics/middleware.js
+++ spot-client/src/spot-remote/analytics/middleware.js
@@ -1,4 +1,5 @@
-import { analytics, feedbackEvents } from 'common/analytics';
+import { analytics, feedbackEvents, inCallEvents } from 'common/analytics';
+import { REMOTE_CONTROL_REQUEST_STATE, requestStates, requestTypes } from 'common/app-state';
 import { MiddlewareRegistry } from 'common/redux';
 import { SUBMIT_FEEDBACK } from './../remote-control';
 
@@ -6,6 +7,10 @@ MiddlewareRegistry.register(() => next => action => {
     const result = next(action);
 
     switch (action.type) {
+    case REMOTE_CONTROL_REQUEST_STATE: {
+        _remoteControlRequestState(action);
+        break;
+    }
     case SUBMIT_FEEDBACK: {
         _submitFeedback(action);
         break;
@@ -14,6 +19,31 @@ MiddlewareRegistry.register(() => next => action => {
 
     return result;
 });
+
+/**
+ * Tracks the {@link REMOTE_CONTROL_REQUEST_STATE} action and logs analytics when the PENDING state
+ * is set (it corresponds to the user triggering an action).
+ *
+ * @param {string} requestType - The type of request like audio mute, video mute etc.
+ * @param {string} requestState - The request state.
+ * @param {any} expectedState - Depends on the request type.
+ * @private
+ * @returns {void}
+ */
+function _remoteControlRequestState({ requestType, requestState, expectedState }) {
+    if (requestState !== requestStates.PENDING) {
+        return;
+    }
+
+    switch (requestType) {
+    case requestTypes.AUDIO_MUTE:
+        analytics.log(inCallEvents.AUDIO_MUTE, { muting: expectedState });
+        break;
+    case requestTypes.VIDEO_MUTE:
+        analytics.log(inCallEvents.VIDEO_MUTE, { muting: expectedState });
+        break;
+    }
+}
 
 /**
  * Sends analytics events for the {@link SUBMIT_FEEDBACK} action.
