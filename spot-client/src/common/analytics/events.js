a8bd2f817c2e84374ab9664bae046b10128805d2
paweldomas <pawel.domas@jitsi.org>
2019-04-18 10:20:02 -0500

    feat(analytics): report audio and video mute events from Spot Remote

--- spot-client/src/common/analytics/events.js
+++ spot-client/src/common/analytics/events.js
@@ -2,3 +2,14 @@ export const feedbackEvents = {
     SKIP: 'feedback-skip',
     SUBMIT: 'feedback-submit'
 };
+
+export const inCallEvents = {
+    AUDIO_MUTE: 'audio-mute',
+    HANG_UP: 'hang-up',
+    SHARE_CONTENT: 'share-content',
+    SCREENSHARE_STOP: 'screenshare-stop',
+    VIDEO_MUTE: 'video-mute',
+    WIRED_SCREENSHARE_START: 'wired-screenshare',
+    WIRELESS_SCREENSHARE_START: 'wireless-screenshare',
+    WIRELESS_SCREENSHARE_START_NO_WIRED_AVAILABLE: 'wireless-only-screenshare'
+};
