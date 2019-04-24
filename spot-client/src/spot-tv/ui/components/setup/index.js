d50968157d211d97e7b655bb949e0fdee5855e51
Leonard Kim <leonard.kim@8x8.com>
2019-04-24 12:34:16 -0700

    ref(setup): move wired screenshare selector to device selection

--- spot-client/src/spot-tv/ui/components/setup/index.js
+++ spot-client/src/spot-tv/ui/components/setup/index.js
@@ -1,4 +1,3 @@
 export { default as Profile } from './profile';
-export { default as ScreenshareInput } from './screenshare-input';
 export { SelectMedia } from './select-media';
 export { default as Setup } from './setup';
