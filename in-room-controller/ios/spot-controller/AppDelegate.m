0fd8a126b5b819360bd749c73d588f95ff55e754
paweldomas <pawel.domas@jitsi.org>
2019-05-07 10:07:52 -0500

    rename ios

--- in-room-controller/ios/spot-controller/AppDelegate.m
+++ in-room-controller/ios/spot-controller/AppDelegate.m
@@ -19,7 +19,7 @@
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"mobile"
+                                                      moduleName:@"spot-controller"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [UIColor blackColor];
