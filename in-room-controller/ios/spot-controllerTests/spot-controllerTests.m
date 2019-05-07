e05ab5fa759ae60435c61e37cce2833cd335ab14
paweldomas <pawel.domas@jitsi.org>
2019-05-07 10:07:52 -0500

    rename ios

--- in-room-controller/ios/spot-controllerTests/spot-controllerTests.m
+++ in-room-controller/ios/spot-controllerTests/spot-controllerTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface mobileTests : XCTestCase
+@interface spotControllerTests : XCTestCase
 
 @end
 
-@implementation mobileTests
+@implementation spotControllerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
