fc2cb651d95630d638e7f9784753ee395880af6b
Leonard Kim <leonard.kim@8x8.com>
2019-05-10 20:17:44 -0700

    ref(tests): replace screen-info with custom screen count utils
    screen-info is no longer being maintained and has an outdated
    package with a security vulnerability.

---
+++ spot-webdriver/screen-info/index.js
@@ -0,0 +1,62 @@
+/**
+ * Private helper to execute a terminal command and return the output.
+ *
+ * @param {string} command - The command to execute.
+ * @private
+ * @returns {string}
+ */
+function _executeCommand(command) {
+    return require('child_process')
+        .execSync(command)
+        .toString();
+}
+
+const screenCountCommands = {
+    /**
+     * The function to call to get a count of connected monitors on mac.
+     *
+     * @returns {number}
+     */
+    darwin: () => parseInt(
+        _executeCommand('system_profiler SPDisplaysDataType | grep -c Resolution'),
+        10
+    ),
+
+    /**
+     * The function to call to get a count of connected monitors on linux.
+     *
+     * @returns {number}
+     */
+    linux: () => parseInt(
+        _executeCommand('xdpyinfo | grep -c resolution'),
+        10
+    ),
+
+    /**
+     * The function to call to get a count of connected monitors on windows.
+     *
+     * @returns {number}
+     */
+    win32: () =>
+        _executeCommand(
+            'wmic path Win32_PnPEntity where "Service=\'Monitor\'" get deviceid /value')
+        .match(/DeviceID/g)
+        .length
+};
+
+module.exports = {
+    /**
+     * Returns a count of how many monitors are connected to the machine.
+     *
+     * @returns {number}
+     */
+    getScreenCount() {
+        const command = screenCountCommands[process.platform];
+
+        if (!command) {
+            throw new Error(`No screen count command for env ${process.platform}`);
+        }
+
+        return command();
+    }
+};
