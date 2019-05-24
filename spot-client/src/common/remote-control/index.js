9652607152246da201ed74eab36c9893ddd4bd6b
Leonard Kim <leonard.kim@8x8.com>
2019-05-24 10:13:03 -0700

    ref(rcs): stop exporting base rcs

--- spot-client/src/common/remote-control/index.js
+++ spot-client/src/common/remote-control/index.js
@@ -1,7 +1,6 @@
 export * from './constants';
 export { default as RemoteControlServiceSubscriber }
     from './remote-control-service-subscriber';
-export { default as remoteControlService } from './remote-control-service';
 export { default as spotRemoteRemoteControlService }
     from './spot-remote-remote-control-service';
 export { default as spotTvRemoteControlService }
