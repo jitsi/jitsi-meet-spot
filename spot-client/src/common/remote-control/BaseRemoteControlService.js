8fe6a3a25201f21db2b538f3aca3297cf4e618de
Leonard Kim <leonard.kim@8x8.com>
2019-06-12 17:24:17 -0700

    update typedefs

--- spot-client/src/common/remote-control/BaseRemoteControlService.js
+++ spot-client/src/common/remote-control/BaseRemoteControlService.js
@@ -36,6 +36,13 @@ export class BaseRemoteControlService extends Emitter {
     }
 
     /**
+     * @typedef {Object} RoomProfile - Public information about the Spot-TV
+     * and the physical room it is hosted in.
+     *
+     * @property {string} [name] - The name of psychical conference room
+     * which has the Spot-TV.
+     */
+    /**
      * Creates a connection to the remote control service.
      *
      * @param {Object} options - Information necessary for creating the MUC.
@@ -49,7 +56,7 @@ export class BaseRemoteControlService extends Emitter {
      * created to automatically update the join code at the provided rate.
      * @param {Object} options.serverConfig - Details on how the XMPP connection
      * should be made.
-     * @returns {Promise<string>}
+     * @returns {Promise<RoomProfile>}
      */
     connect(options) {
         // Keep a cache of the initial options for reference when reconnecting.
@@ -207,7 +214,9 @@ export class BaseRemoteControlService extends Emitter {
 
     /**
      * @typedef {Object} RoomInfo
-     * @property {string} roomName - the name of the room.
+     * @property {string} [name] - The name of psychical conference room
+     * which has the Spot-TV.
+     * @property {string} roomName - the name of the MUC room.
      * @property {string} [roomLock] - the room's password (if any).
      */
     /**
