87248edc90ae61e3780555a4e861bc34b4e1bf46
paweldomas <pawel.domas@jitsi.org>
2019-06-20 16:49:53 -0500

    ref: move the connection dropped case to be handled in action
    The withRemoteControl loader will no longer track Spot TV presence and
    trigger disconnect when TV is not in MUC, but the RCS service will go
    through regular disconnect flow.

--- spot-client/src/spot-remote/ui/loaders/withRemoteControl.js
+++ spot-client/src/spot-remote/ui/loaders/withRemoteControl.js
@@ -1,13 +1,6 @@
-import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
 
-import {
-    addNotification,
-    isConnectedToSpot
-} from 'common/app-state';
 import { history } from 'common/history';
-import { logger } from 'common/logger';
 import { remoteControlClient } from 'common/remote-control';
 import { ROUTES } from 'common/routing';
 import { AbstractLoader, generateWrapper } from 'common/ui';
@@ -19,12 +12,6 @@ import { AbstractLoader, generateWrapper } from 'common/ui';
  * @extends React.Component
  */
 export class RemoteControlLoader extends AbstractLoader {
-    static propTypes = {
-        ...AbstractLoader.propTypes,
-        isConnectedToSpot: PropTypes.bool,
-        onUnexpectedDisconnected: PropTypes.func
-    };
-
     /**
      * Initializes a new {@code RemoteControlLoader} instance.
      *
@@ -33,18 +20,6 @@ export class RemoteControlLoader extends AbstractLoader {
      */
     constructor(props) {
         super(props, 'SpotRemote');
-    }
-
-    /**
-     * Navigates away from the view {@code RemoteControl} when no longer
-     * connected to a Spot-TV.
-     *
-     * @inheritdoc
-     */
-    componentDidUpdate() {
-        if (!this.props.isConnectedToSpot) {
-            this.props.onUnexpectedDisconnected();
-        }
     }
 
     /**
@@ -78,42 +53,6 @@ export class RemoteControlLoader extends AbstractLoader {
     }
 }
 
-/**
- * Selects parts of the Redux state to pass in with the props of
- * {@code RemoteControls}.
- *
- * @param {Object} state - The Redux state.
- * @private
- * @returns {Object}
- */
-function mapStateToProps(state) {
-    return {
-        isConnectedToSpot: isConnectedToSpot(state)
-    };
-}
-
-/**
- * Creates actions which can update Redux state.
- *
- * @param {Function} dispatch - The Redux dispatch function to update state.
- * @private
- * @returns {Object}
- */
-function mapDispatchToProps(dispatch) {
-    return {
-        /**
-         * Adds a notification that an unexpected disconnect has occurred.
-         *
-         * @returns {void}
-         */
-        onUnexpectedDisconnected() {
-            logger.log('onUnexpectedDisconnect');
-            dispatch(addNotification('error', 'Disconnected'));
-            history.push(ROUTES.CODE);
-        }
-    };
-}
-
-const ConnectedRemoteControlLoader = withRouter(connect(mapStateToProps, mapDispatchToProps)(RemoteControlLoader));
+const ConnectedRemoteControlLoader = withRouter(RemoteControlLoader);
 
 export default generateWrapper(ConnectedRemoteControlLoader);
