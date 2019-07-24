b5ebf8717b3a267bea41311a3c50ea0b3ff2acee
Leonard Kim <leonard.kim@8x8.com>
2019-07-24 09:57:54 -0700

    feat(backend): add an id to requests

--- spot-client/setupTests.js
+++ spot-client/setupTests.js
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 import Enzyme from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
@@ -6,3 +7,7 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
 jest.mock('./src/common/logger');
 
 global.fetch = require('jest-fetch-mock');
+
+global.crypto = {
+    getRandomValues: buffer => crypto.randomFillSync(buffer)
+};
