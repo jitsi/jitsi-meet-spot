96f39a6ddad108950398988c7ad3e49149c2a598
Leonard Kim <leonard.kim@8x8.com>
2019-09-10 15:31:02 -0700

    feat(dial): press 0 for +

--- spot-client/src/spot-remote/ui/components/dial-pad/dial-button.js
+++ spot-client/src/spot-remote/ui/components/dial-pad/dial-button.js
@@ -9,8 +9,9 @@ import React from 'react';
  */
 export default class DialButton extends React.Component {
     static propTypes = {
-        main: PropTypes.string,
+        mainValue: PropTypes.string,
         onClick: PropTypes.func,
+        onLongClick: PropTypes.func,
         sub: PropTypes.string
     }
 
@@ -23,7 +24,21 @@ export default class DialButton extends React.Component {
     constructor(props) {
         super(props);
 
-        this._onClick = this._onClick.bind(this);
+        this._submitSecondaryValueTimeout = null;
+
+        this._onClearSecondaryValueSubmitTimeout
+            = this._onClearSecondaryValueSubmitTimeout.bind(this);
+        this._onTouchEnd = this._onTouchEnd.bind(this);
+        this._onTouchStart = this._onTouchStart.bind(this);
+    }
+
+    /**
+     * Stops any long press timeout in flight.
+     *
+     * @inheritdoc
+     */
+    componentWillUnmount() {
+        this._onClearSecondaryValueSubmitTimeout();
     }
 
     /**
@@ -36,9 +51,13 @@ export default class DialButton extends React.Component {
         return (
             <button
                 className = 'dial-button'
-                onClick = { this._onClick }
+                onClick = { this._onClearSecondaryValueSubmitTimeout }
+                onMouseDown = { this._onTouchStart }
+                onTouchEnd = { this._onTouchEnd }
+                onTouchStart = { this._onTouchStart }
+                tabIndex = { -1 }
                 type = 'button'>
-                <span className = 'main'>{ this.props.main }</span>
+                <span className = 'main'>{ this.props.mainValue }</span>
                 { typeof this.props.sub === 'undefined'
                     ? null
                     : <span className = 'sub'>{ this.props.sub }</span> }
@@ -47,12 +66,62 @@ export default class DialButton extends React.Component {
     }
 
     /**
-     * Invokes the passed in onClick callback when the component is clicked.
+     * Stops the timeout which will invoke the onLongClick prop callback when
+     * the button is long pressed.
      *
      * @private
      * @returns {void}
      */
-    _onClick() {
-        this.props.onClick(this.props.main);
+    _onClearSecondaryValueSubmitTimeout() {
+        clearTimeout(this._submitSecondaryValueTimeout);
+    }
+
+    /**
+     * Stops any long press handling.
+     *
+     * @param {Object} e - The React SyntheticEvent for when the user stops a
+     * touching the window.
+     * @private
+     * @returns {void}
+     */
+    _onTouchEnd(e) {
+        // Use preventDefault to avoid an extra call to onMouseDown.
+        // See https://github.com/facebook/react/issues/9809#issuecomment-507640770
+        e.preventDefault();
+
+        this._onClearSecondaryValueSubmitTimeout();
+    }
+
+    /**
+     * Callback invoked when the user interacts with the button to simulate a
+     * click.
+     *
+     * @param {Object} e - The React SyntheticEvent for when the user has
+     * initially touched the button.
+     * @private
+     * @returns {void}
+     */
+    _onTouchStart(e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        this._setSecondaryValueSubmitTimeout();
+        this.props.onClick(this.props.mainValue);
+    }
+
+    /**
+     * Starts the timeout for checking if the button is being long pressed.
+     *
+     * @private
+     * @returns {void}
+     */
+    _setSecondaryValueSubmitTimeout() {
+        this._onClearSecondaryValueSubmitTimeout();
+
+        this._submitSecondaryValueTimeout = setTimeout(() => {
+            if (this.props.onLongClick) {
+                this.props.onLongClick(this.props.sub);
+            }
+        }, 1500);
     }
 }
