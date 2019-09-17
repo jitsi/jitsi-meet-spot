f5a7dc3e84c2aac1ba141f8a3e81b3f185122cfc
Saúl Ibarra Corretgé <s@saghul.net>
2019-09-17 22:57:19 +0200

    ref(bootstrap): auto-detect app basename

--- spot-client/src/common/history/index.js
+++ spot-client/src/common/history/index.js
@@ -1,3 +1,17 @@
 import { createBrowserHistory } from 'history';
 
-export const history = createBrowserHistory();
+/**
+ * Gets the basename (aka subdirectory) where the app is running.
+ *
+ * @returns {string} The app's basename.
+ */
+function getBaseName() {
+    const { pathname } = window.location;
+    const idx = pathname.lastIndexOf('/');
+
+    return idx === -1 ? '/' : pathname.substring(0, idx + 1);
+}
+
+export const history = createBrowserHistory({
+    basename: getBaseName()
+});
