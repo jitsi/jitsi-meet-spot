d936b9c280842f7d8b54c0e11813faf4bb0d2063
Leonard Kim <leonard.kim@8x8.com>
2019-09-20 15:40:05 -0700

    fix(dtmf): show entered values

--- spot-client/src/spot-remote/ui/components/dial-pad/NumberInput.js
+++ spot-client/src/spot-remote/ui/components/dial-pad/NumberInput.js
@@ -10,8 +10,32 @@ export default class NumberInput extends React.Component {
     static propTypes = {
         onChange: PropTypes.func,
         placeholder: PropTypes.string,
+        readOnly: PropTypes.bool,
         value: PropTypes.string
     };
+
+    /**
+     * Initializes a new {@code NumberInput} instance.
+     *
+     * @param {Object} props - The read-only properties with which the new
+     * instance is to be initialized.
+     */
+    constructor(props) {
+        super(props);
+
+        this._inputRef = React.createRef();
+    }
+
+    /**
+     * Ensures the latest value is displayed if in read only mode.
+     *
+     * @inheritdoc
+     */
+    componentDidUpdate(prevProps) {
+        if (this.props.readOnly && prevProps.value !== this.props.value) {
+            this._inputRef.current.scrollLeft = this._inputRef.current.scrollWidth;
+        }
+    }
 
     /**
      * Implements React's {@link Component#render()}.
@@ -23,9 +47,10 @@ export default class NumberInput extends React.Component {
         return (
             <div className = 'number-input'>
                 <input
-                    className = 'number-input'
                     onChange = { this.props.onChange }
                     placeholder = { this.props.placeholder }
+                    readOnly = { this.props.readOnly }
+                    ref = { this._inputRef }
                     type = 'tel'
                     value = { this.props.value } />
             </div>
