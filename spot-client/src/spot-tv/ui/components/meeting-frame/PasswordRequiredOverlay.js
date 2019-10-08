30d40b798cde56c8b7279b9de6183a376b8b58fa
Leonard Kim <leonard.kim@8x8.com>
2019-10-07 17:34:09 -0700

    ref(meeting): TV password overlay uses i18n

---
+++ spot-client/src/spot-tv/ui/components/meeting-frame/PasswordRequiredOverlay.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { withTranslation } from 'react-i18next';
+
+import { StatusOverlay } from 'common/ui';
+
+/**
+ * Renders a text overlay which hides Jitsi-Meet iFrame when joining a meeting
+ * requires a password be entered.
+ *
+ * @param {Object} props - The read-only properties with which the new
+ * instance is to be initialized.
+ * @returns {ReactElement}
+ */
+export function PasswordRequiredOverlay({ t }) {
+    return (
+        <StatusOverlay title = { t('conferenceStatus.passwordRequired') }>
+            <div>{ t('conferenceStatus.howToEnterPassword') }</div>
+        </StatusOverlay>
+    );
+}
+
+PasswordRequiredOverlay.propTypes = {
+    t: PropTypes.func
+};
+
+export default withTranslation()(PasswordRequiredOverlay);
