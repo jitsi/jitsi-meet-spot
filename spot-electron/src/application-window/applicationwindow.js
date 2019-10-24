8aee113fbe8b472e16d2a7e79bdbeeed0f744f20
Leonard Kim <leonard.kim@8x8.com>
2019-10-24 12:40:57 -0700

    ref(electron): use kiosk mode
    Instead of relying on manual full screen mode.
    This will remove the need for saving full screen
    preference and window size prefernces.

--- spot-electron/src/application-window/applicationwindow.js
+++ spot-electron/src/application-window/applicationwindow.js
@@ -1,14 +1,10 @@
 const {
     BrowserWindow
 } = require('electron');
-const process = require('process');
+const isDev = require('electron-is-dev');
 
-const {
-    defaultSpotURL,
-    spashScreen
-} = require('../../config');
+const { defaultSpotURL } = require('../../config');
 
-const { config } = require('../config');
 const { logger } = require('../logger');
 
 /**
@@ -25,54 +21,22 @@ const SPOT_ELECTRON_FEATURE_VERSION = 'SpotElectron/1';
 let applicationWindow;
 
 /**
- * Reference to the splash window. This can be disabled in the config.
- */
-let splashWindow;
-
-/**
  * Function to create the main window (application window).
  *
  * @returns {void}
  */
 function createApplicationWindow() {
-    const startFullScreen = config.getValue('window.fullscreen');
-
     applicationWindow = new BrowserWindow({
-        fullscreen: startFullScreen,
-        height: config.getValue('window.height'),
+        // By default the application should always run in kiosk mode.
+        kiosk: !isDev,
         show: false,
-        titleBarStyle: 'hidden',
-        width: config.getValue('window.width'),
         webPreferences: {
             nodeIntegration: true
         }
     });
 
-    if (spashScreen && !startFullScreen) {
-        splashWindow = new BrowserWindow({
-            frame: false,
-            height: spashScreen.height,
-            maximizable: false,
-            parent: applicationWindow,
-            resizable: false,
-            width: spashScreen.width
-        });
-
-        splashWindow.loadFile(spashScreen.logo);
-    }
-
     // Set event handlers
     applicationWindow.once('ready-to-show', _onWindowReady);
-    applicationWindow.on('resize', _onWindowResize);
-    applicationWindow.on('enter-full-screen', () => {
-        applicationWindow.setMenuBarVisibility(false);
-        config.setValue('window.fullscreen', true);
-    });
-    applicationWindow.on('leave-full-screen', () => {
-        applicationWindow.setMenuBarVisibility(true);
-        config.setValue('window.fullscreen', undefined);
-    });
-
     applicationWindow.webContents.on('crashed', () => {
         // The setTimeout 0 is necessary clear the current stack or else
         // navigation will cause the main app to crash as well.
@@ -90,21 +54,6 @@ function createApplicationWindow() {
         userAgent: `${applicationWindow.webContents.getUserAgent()} ${SPOT_ELECTRON_FEATURE_VERSION}`
     });
 
-    applicationWindow.webContents.on('did-finish-load', () => {
-        if (process.platform === 'darwin') {
-            /* eslint-disable max-len */
-            const code = `
-                var _titleBar = document.createElement('div');
-                _titleBar.id = 'titleBar';
-                _titleBar.setAttribute('style', '-webkit-app-region: drag; position: fixed; z-index: 9999; top: 0; left: 0; width: 100%; height: 32px; min-height: 32px;');
-                document.getElementsByTagName('body')[0].prepend(_titleBar);
-            `;
-
-            /* eslint-enable max-len */
-            applicationWindow.webContents.executeJavaScript(code);
-        }
-    });
-
     logger.info(`Spot started with Spot-TV URL ${defaultSpotURL}`);
 }
 
@@ -114,23 +63,7 @@ function createApplicationWindow() {
  * @returns {void}
  */
 function _onWindowReady() {
-    splashWindow && splashWindow.destroy();
-
-    applicationWindow.setMenuBarVisibility(!config.getValue('window.fullscreen'));
-
     applicationWindow.show();
-}
-
-/**
- * Callback to persist window size on resizing the main window.
- *
- * @returns {void}
- */
-function _onWindowResize() {
-    const [ width, height ] = applicationWindow.getSize();
-
-    config.setValue('window.height', height);
-    config.setValue('window.width', width);
 }
 
 module.exports = {
