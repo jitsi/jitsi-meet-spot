9b7f50750493bcf026cdee5d529147851ec86a96
paweldomas <pawel.domas@jitsi.org>
2019-11-01 13:57:13 -0500

    feat(electron): use 'electron-log' to write to a log file

--- spot-electron/src/spot-client-log-transport/SpotClientLogTransport.js
+++ spot-electron/src/spot-client-log-transport/SpotClientLogTransport.js
@@ -1,4 +1,26 @@
+const electronLog = require('electron-log');
+
 const { clientController, events } = require('../client-control');
+
+// Make the electron-log accept everything.
+electronLog.transports.file.level = 'verbose';
+
+// Configure electron-log to write to file only
+electronLog.transports.console = undefined;
+electronLog.transports.mainConsole = undefined;
+electronLog.transports.rendererConsole = undefined;
+
+// Uncomment this line to try out electron file logger in dev
+// electronLog.transports.file.file = './spot-electron-dev.log';
+
+const JITSI_TO_ELECTRON_LEVELS = {
+    'error': 'error',
+    'warn': 'warn',
+    'info': 'info',
+    'trace': 'verbose',
+    'debug': 'debug',
+    'log': 'log'
+};
 
 /**
  * Log levels supported by the jitsi logger.
@@ -50,6 +72,8 @@ class SpotClientLogTransport {
     _logImpl(level, loggerName, message, context) {
         const msgString = `${loggerName} ${message}`;
 
+        this._logToElectronLog(level, loggerName, message, context);
+
         if (clientController.canSendClientMessage()) {
             clientController.sendClientMessage('spot-electron-logs', level, msgString, context);
         } else {
@@ -62,6 +86,27 @@ class SpotClientLogTransport {
                 message: msgString,
                 context
             });
+        }
+    }
+
+    /**
+     * Forwards the log message to the 'electron-log' in order to write to the log file.
+     *
+     * @param {string} level - The jitsi logging level.
+     * @param {string} loggerName - Logger name(as defined by jitsi-logger lib).
+     * @param {string} message - The text message to be logged.
+     * @param {Object} context - JSON compatible context object with extra info.
+     * @private
+     * @returns {void}
+     */
+    _logToElectronLog(level, loggerName, message, context) {
+        const logMethod = electronLog[JITSI_TO_ELECTRON_LEVELS[level]];
+
+        if (logMethod) {
+            logMethod && logMethod(message, JSON.stringify(context));
+        } else {
+            // eslint-disable-next-line no-console
+            console.error(`Failed to map jitsi to electron log level:${level}`);
         }
     }
 }
