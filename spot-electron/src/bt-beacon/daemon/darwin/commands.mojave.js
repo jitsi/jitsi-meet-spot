b677bb75a726f0bfde1d019259b5df51ceea5ff7
Bettenbuk Zoltan <zoltan.bettenbuk@gmail.com>
2019-11-13 12:24:18 +0100

    feat: beacon catalina support

---
+++ spot-electron/src/bt-beacon/daemon/darwin/commands.mojave.js
@@ -0,0 +1,10 @@
+module.exports = {
+    beaconActivated: 28,
+    beaconDeactivated: 29,
+    connectionRequest: 67,
+    deactivateBeacon: 18,
+    initializeDevice: 1,
+    ready: 38,
+    activateBeacon: 17,
+    statusUpdate: 4
+};
--- spot-electron/src/bt-beacon/daemon/darwin/index.js
+++ spot-electron/src/bt-beacon/daemon/darwin/index.js
@@ -1,5 +1,1 @@
-const os = require('os');
-const osRelease = parseFloat(os.release());
-
-// older macOS support comes later
-module.exports = osRelease >= 18 ? require('./bluetoothd') : undefined;
+module.exports = require('./bluetoothd');
