28515791763d4560e80656365d7fd067ffbb7469
Leonard Kim <leonard.kim@8x8.com>
2020-01-31 10:58:56 -0800

    ref(settings): convert menu option to function

--- spot-client/src/spot-tv/ui/components/admin/admin-entry.js
+++ spot-client/src/spot-tv/ui/components/admin/admin-entry.js
@@ -3,29 +3,25 @@ import React from 'react';
 
 /**
  * Implements a component that renders an admin modal entry.
+ *
+ * @param {Object} props - The read-only properties with which the new
+ * instance is to be initialized.
+ * @returns {ReactElement}
  */
-export default class AdminEntry extends React.Component {
-    static propTypes = {
-        children: PropTypes.any,
-        entryLabel: PropTypes.string
-    }
-
-    /**
-     * Implements React's {@link Component#render()}.
-     *
-     * @inheritdoc
-     * @returns {ReactElement}
-     */
-    render() {
-        return (
-            <div className = 'admin-entry'>
-                <div className = 'admin-entry-title'>
-                    { this.props.entryLabel }
-                </div>
-                <div className = 'admin-entry-content'>
-                    { this.props.children }
-                </div>
+export default function AdminEntry(props) {
+    return (
+        <div className = 'admin-entry'>
+            <div className = 'admin-entry-title'>
+                { props.entryLabel }
             </div>
-        );
-    }
+            <div className = 'admin-entry-content'>
+                { props.children }
+            </div>
+        </div>
+    );
 }
+
+AdminEntry.propTypes = {
+    children: PropTypes.any,
+    entryLabel: PropTypes.string
+};
