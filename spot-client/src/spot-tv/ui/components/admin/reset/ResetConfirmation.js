d366b309af76385be3850dced59699f964f67844
Leonard Kim <leonard.kim@8x8.com>
2020-02-03 10:21:58 -0800

    ref(admin): convert reset to use hooks

--- spot-client/src/spot-tv/ui/components/admin/reset/ResetConfirmation.js
+++ spot-client/src/spot-tv/ui/components/admin/reset/ResetConfirmation.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { withTranslation } from 'react-i18next';
 
 import { logger } from 'common/logger';
@@ -11,135 +11,65 @@ import { Button } from 'common/ui/components/button';
  * Displays a menu option, with confirmation, to clear all saved Spot state,
  * including any spot-tv setup.
  *
- * @extends React.Component
+ * @param {Object} props - The read-only properties with which the new
+ * instance is to be initialized.
+ * @returns {ReactElement}
  */
-export class ResetConfirmation extends React.Component {
-    static propTypes = {
-        t: PropTypes.func
-    };
+export function ResetConfirmation({ t }) {
+    const [ isResetting, setIsResetting ] = useState(false);
+    const [ showResetConfirmation, setShowResetConfirmation ] = useState(false);
 
-    state = {
-        resetting: false,
-        showResetConfirm: false
-    };
+    const onResetApp = useCallback(() => {
+        logger.log('confirmed reset of application');
 
-    /**
-     * Initializes a new {@code ResetConfirmation} instance.
-     *
-     * @param {Object} props - The read-only properties with which the new
-     * instance is to be initialized.
-     */
-    constructor(props) {
-        super(props);
+        setIsResetting(true);
 
-        this._onResetApp = this._onResetApp.bind(this);
-        this._onShowResetButton = this._onShowResetButton.bind(this);
-        this._onShowResetConfirm = this._onShowResetConfirm.bind(this);
-    }
+        clearPersistedState();
 
-    /**
-     * Implements React's {@link Component#render()}.
-     *
-     * @inheritdoc
-     */
-    render() {
-        let child;
+        setTimeout(() => {
+            logger.log('reset reloading application');
 
-        if (this.state.resetting) {
-            child = this.props.t('admin.resetting');
-        } else {
-            child = this.state.showResetConfirm
-                ? this._renderResetConfirm()
-                : this._renderResetButton();
-        }
+            window.location.reload();
+        }, 2000);
+    }, [ setIsResetting ]);
+    const onToggleResetConfirmation = useCallback(() => {
+        setShowResetConfirmation(!showResetConfirmation);
+    }, [ showResetConfirmation ]);
 
-        return (
-            <div className = 'admin-content'>
-                { child }
+    let child;
+
+    if (isResetting) {
+        child = t('admin.resetting');
+    } else if (showResetConfirmation) {
+        child = (
+            <div>
+                <Button onClick = { onResetApp }>
+                    { t('buttons.confirm') }
+                </Button>
+                <Button
+                    appearance = 'secondary'
+                    onClick = { onToggleResetConfirmation }>
+                    { t('buttons.cancel') }
+                </Button>
             </div>
         );
-    }
-
-    /**
-     * Creates a new React Element which starts the Spot state reset
-     * flow.
-     *
-     * @private
-     * @returns {ReactElement}
-     */
-    _renderResetButton() {
-        return (
-            <Button onClick = { this._onShowResetConfirm }>
-                { this.props.t('admin.reset') }
+    } else {
+        child = (
+            <Button onClick = { onToggleResetConfirmation }>
+                { t('admin.reset') }
             </Button>
         );
     }
 
-    /**
-     * Creates a new React Element asking for confirmation before resetting Spot
-     * state and shows a button to complete the reset.
-     *
-     * @private
-     * @returns {ReactElement}
-     */
-    _renderResetConfirm() {
-        return (
-            <div>
-                <Button onClick = { this._onResetApp }>
-                    { this.props.t('buttons.confirm') }
-                </Button>
-                <Button
-                    appearance = 'secondary'
-                    onClick = { this._onShowResetButton }>
-                    { this.props.t('buttons.cancel') }
-                </Button>
-            </div>
-        );
-    }
-
-    /**
-     * Clears saved Spot state.
-     *
-     * @private
-     * @returns {void}
-     */
-    _onResetApp() {
-        logger.log('reset confirmed reset of application');
-
-        this.setState({
-            resetting: true
-        }, () => {
-            logger.log('reset clearing application state');
-
-            clearPersistedState();
-
-            setTimeout(() => {
-                logger.log('reset reloading application');
-
-                window.location.reload();
-            }, 2000);
-        });
-    }
-
-    /**
-     * Sets the internal state to display the UI to start the Spot reset flow.
-     *
-     * @private
-     * @returns {void}
-     */
-    _onShowResetButton() {
-        this.setState({ showResetConfirm: false });
-    }
-
-    /**
-     * Sets the internal state to display the UI to execute the Spot reset flow.
-     *
-     * @private
-     * @returns {void}
-     */
-    _onShowResetConfirm() {
-        this.setState({ showResetConfirm: true });
-    }
+    return (
+        <div className = 'admin-content'>
+            { child }
+        </div>
+    );
 }
+
+ResetConfirmation.propTypes = {
+    t: PropTypes.func
+};
 
 export default withTranslation()(ResetConfirmation);
