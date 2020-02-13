269913816b1d4642a7c9e5a1573048eca7d2d95e
Bettenbuk Zoltan <zoltan.bettenbuk@gmail.com>
2020-02-13 14:29:34 +0100

    signal llpc through the API

--- spot-client/src/spot-tv/native-functions/native-controller/middleware.js
+++ spot-client/src/spot-tv/native-functions/native-controller/middleware.js
@@ -2,7 +2,7 @@
 import { SPOT_TV_SET_REMOTE_JOIN_CODE, SPOT_TV_SET_STATE } from 'common/app-state';
 import { BOOTSTRAP_COMPLETE } from 'common/app-state/bootstrap';
 import { MiddlewareRegistry } from 'common/redux';
-
+import { SET_LONG_LIVED_PAIRING_CODE_INFO, getLongLivedPairingCodeInfo } from 'spot-tv/backend';
 import nativeController from './native-controller';
 
 /**
@@ -11,12 +11,16 @@ import nativeController from './native-controller';
  * @param {Store} store - The redux store.
  * @returns {Function}
  */
-MiddlewareRegistry.register(() => next => action => {
+MiddlewareRegistry.register(({ getState }) => next => action => {
     const result = next(action);
 
     switch (action.type) {
     case BOOTSTRAP_COMPLETE:
         nativeController._sendSpotClientReady();
+        sendLongLivedPairingCode(getLongLivedPairingCodeInfo(getState()));
+        break;
+    case SET_LONG_LIVED_PAIRING_CODE_INFO:
+        sendLongLivedPairingCode(action.longLivedPairingCodeInfo);
         break;
     case SPOT_TV_SET_REMOTE_JOIN_CODE:
         nativeController.sendMessage('updateJoinCode', {
@@ -30,6 +34,18 @@ MiddlewareRegistry.register(() => next => action => {
 
     return result;
 });
+
+/**
+ * Sends the long lived pairing code through the external API when updated.
+ *
+ * @param {Object} codeInfo - The pairing code info we have on record.
+ * @returns {void}
+ */
+function sendLongLivedPairingCode(codeInfo) {
+    nativeController.sendMessage('updateLongLivedPairingCode', {
+        longLivedPairingCode: (codeInfo || {}).code
+    });
+}
 
 /**
  * Sends part of the new state through the external API if necessary.
