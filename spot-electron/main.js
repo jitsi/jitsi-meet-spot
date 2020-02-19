5ba5c24234857994764ea2cdf894e6fb6002b3d3
paweldomas <pawel.domas@jitsi.org>
2020-02-19 12:51:19 -0600

    WiP permissions error

--- spot-electron/main.js
+++ spot-electron/main.js
@@ -1,9 +1,23 @@
 require('electron-is-dev') && require('dotenv').config();
 
-const { app } = require('electron');
+const { app, dialog } = require('electron');
+const { exec } = require('child_process');
 
 app.setLoginItemSettings({
     openAtLogin: true
+});
+
+const execute = command => new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            reject({
+                error,
+                stderr
+            });
+        } else {
+            resolve(stdout);
+        }
+    });
 });
 
 const { createApplicationWindow } = require('./src/application-window');
@@ -18,4 +32,19 @@ require('./src/client-control');
 require('./src/exit');
 require('./src/volume-control');
 
-app.on('ready', createApplicationWindow);
+app.on('ready', () => {
+    const appPath = app.getAppPath();
+
+    console.info(`Checking write access to the app path: ${appPath}`);
+
+    // FIXME work in progress - works on Mac only
+    execute(`test -w ${appPath}`).then(() => {
+        createApplicationWindow();
+    }, error => {
+        console.error('The app path is not writeable', { error });
+        dialog.showErrorBox(
+            'Permission error',
+            `The user doesn't have write access to the application folder. Error: ${JSON.stringify(error)}`);
+        app.quit();
+    });
+});
