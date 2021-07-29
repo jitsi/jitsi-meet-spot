837373b486e949969061114f23839c20049fe110
Saúl Ibarra Corretgé <saghul@jitsi.org>
2021-07-29 16:10:12 +0200

    feat(electron) add ability to enable devtools
    Opening SpotTV with `--show-devtools` will start the application with
    the devtools dock panel opened.

--- spot-electron/main.js
+++ spot-electron/main.js
@@ -1,10 +1,22 @@
 require('electron-is-dev') && require('dotenv').config();
 
 const { app } = require('electron');
+const isDev = require('electron-is-dev');
 
 app.setLoginItemSettings({
     openAtLogin: true
 });
+
+// Helper for troubleshooting.
+const debug = require('electron-debug');
+const process = require('process');
+
+if (isDev || process.argv.indexOf('--show-devtools') !== -1) {
+    debug({
+        isEnabled: true,
+        showDevTools: true
+    });
+}
 
 const { createApplicationWindow } = require('./src/application-window');
 
