9391a1ecd34ed600927110fd1dca761285a2e643
Saúl Ibarra Corretgé <saghul@jitsi.org>
2021-07-29 16:10:12 +0200

    feat(electron) add ability to enable devtools
    Opening SpotTV with `--show-devtools` will start the application with
    the devtools dock panel opened.

--- spot-electron/src/exit/exitHandler.js
+++ spot-electron/src/exit/exitHandler.js
@@ -3,4 +3,7 @@ const { clientController } = require('../client-control');
 
 clientController.on('exitApp', () => {
     app.quit();
+
+    // Forceully exit with a 5s timeout.
+    setTimeout(() => app.exit(0), 5000);
 });
