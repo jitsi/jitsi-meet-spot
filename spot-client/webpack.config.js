01fbdc7149f275b738fc000f5221a71d9369768d
Bogdan Duduman <bogdan.duduman@8x8.com>
2022-12-16 11:46:51 +0200

    Add necesarry changes to compile using latest webpack 5

--- spot-client/webpack.config.js
+++ spot-client/webpack.config.js
@@ -8,84 +8,92 @@ const Dotenv = require('dotenv-webpack');
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-    devServer: {
-        compress: true,
-        contentBase: path.join(__dirname, '/'),
-        historyApiFallback: true,
-        host: '0.0.0.0',
-        port: 8000,
-        publicPath: '/dist/'
-    },
-    devtool: 'source-map',
-    entry: {
-        app: './src/index.js'
-    },
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                exclude: /(node_modules)/,
-                loader: 'eslint-loader',
-                test: /\.js$/
+module.exports = () => {
+    const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+    return {
+        devServer: {
+            compress: true,
+            historyApiFallback: true,
+            host: '0.0.0.0',
+            port: 8000,
+            static: {
+                directory: path.join(__dirname, '/'),
             },
-            {
-                test: /\.(css|scss)$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            devMiddleware: {
+                publicPath: '/dist/',
             },
-            {
-                test: /\.(jpg|gif)$/,
-                loader: 'file-loader'
-            },
-            {
-                test: /\.(svg|ttf|eot|woff)$/,
-                loader: 'url-loader'
-            },
-            {
-                loader: 'babel-loader',
-                options: {
-                    plugins: [
-                        require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'),
-                        require.resolve('@babel/plugin-proposal-optional-chaining')
-                    ]
-                },
-                test: /\.js$/
-            }
-        ]
-    },
-    plugins: [
-        new CircularDependencyPlugin({
-            exclude: /node_modules/,
-            failOnError: true
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
+        },
+        devtool: 'source-map',
+        entry: {
+            app: './src/index.js',
+        },
+        mode,
+        module: {
+            rules: [
                 {
-                    from: './static',
-                    to: '.'
+                    enforce: 'pre',
+                    exclude: /(node_modules)/,
+                    loader: 'eslint-loader',
+                    test: /\.js$/,
                 },
                 {
-                    from: './config.js',
-                    to: './config'
-                }
-            ]
-        }),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-        }),
-        new Dotenv({
-            systemvars: true // Respect existing environment variables
-        }),
-        new DuplicatesPlugin({
-            emitErrors: true
-        }),
-        new WriteFilePlugin()
-    ],
-    output: {
-        path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        modules: [ path.resolve('./src'), path.resolve('./node_modules') ]
-    }
+                    test: /\.(css|scss)$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader'],
+                },
+                {
+                    test: /\.(jpg|gif)$/,
+                    loader: 'file-loader',
+                },
+                {
+                    test: /\.(svg|ttf|eot|woff)$/,
+                    type: 'asset/resource'
+                },
+                {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'),
+                            require.resolve('@babel/plugin-proposal-optional-chaining'),
+                        ],
+                    },
+                    test: /\.js$/,
+                },
+            ],
+        },
+        plugins: [
+            new CircularDependencyPlugin({
+                exclude: /node_modules/,
+                failOnError: true,
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: './static',
+                        to: '.',
+                    },
+                    {
+                        from: './config.js',
+                        to: './config',
+                    },
+                ],
+            }),
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify(mode),
+            }),
+            new Dotenv({
+                systemvars: true, // Respect existing environment variables
+            }),
+            new DuplicatesPlugin({
+                emitErrors: true,
+            }),
+            new WriteFilePlugin(),
+        ],
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+        },
+        resolve: {
+            modules: [path.resolve('./src'), path.resolve('./node_modules')],
+        },
+    };
 };
