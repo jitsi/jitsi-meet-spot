057ae465edcf36f8fa5b889c649dba8fb6d1d208
Bogdan Duduman <bogdan.duduman@8x8.com>
2022-12-23 11:46:16 +0200

    Remove dev comment

--- spot-client/babel.config.js
+++ spot-client/babel.config.js
@@ -18,8 +18,6 @@ module.exports = {
     ],
     presets: [ '@babel/preset-env', '@babel/preset-react' ],
 
-    // plugins: [],
-
     env: {
         production: {
             plugins: [ 'transform-react-remove-prop-types' ]
