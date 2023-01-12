7af16b12c420fd13c82a30b673b2fd5ed5b7c15d
Bogdan Duduman <bogdan.duduman@8x8.com>
2023-01-12 16:49:32 +0200

    Revert "for testing: set always kickTemporaryRemotes on false"
    This reverts commit 64fa88ec9d772324279a8e28adc37316b26a2a5c.

--- spot-client/src/spot-tv/ui/views/meeting.js
+++ spot-client/src/spot-tv/ui/views/meeting.js
@@ -345,7 +345,7 @@ function mapStateToProps(state) {
         maxDesktopSharingFramerate,
         meetingJoinTimeout: getMeetingJoinTimeout(state),
         minDesktopSharingFramerate,
-        kickTemporaryRemotesOnMeetingEnd: false,
+        kickTemporaryRemotesOnMeetingEnd: isBackendEnabled(state),
         showKickedOverlay: kicked,
         showPasswordPrompt: needPassword,
         preferredCamera: getPreferredCamera(state),
