0c5368bafd096db5a59840135dda54bf263a6af2
Saúl Ibarra Corretgé <s@saghul.net>
2023-08-16 16:36:15 +0200

    fix(client,docker) refactor build

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -8,14 +8,8 @@ fi
 DOCKER_REPO=103425057857.dkr.ecr.us-west-2.amazonaws.com/jitsi/spot-client
 
 cd spot-client
-rm -rf node_modules
-rm -rf dist
-npm install
-export SPOT_CLIENT_VERSION=$TAG
-npm run build:prod
 
-echo "{ \"spotClientVersion\": \"$TAG\" }" > ./dist/spot-client-version.json
+docker build --build-arg SPOT_CLIENT_VERSION=$TAG -t $DOCKER_REPO:$TAG --platform linux/amd64 .
 
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 103425057857.dkr.ecr.us-west-2.amazonaws.com
-docker build -t $DOCKER_REPO:$TAG --platform linux/amd64 .
 docker push $DOCKER_REPO:$TAG
