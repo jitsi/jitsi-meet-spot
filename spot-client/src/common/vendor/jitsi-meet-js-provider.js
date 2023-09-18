e6e53aab75754db6edbde48b06eb6861c00f9b3b
Saúl Ibarra Corretgé <s@saghul.net>
2023-09-18 21:37:12 +0200

    feat(client) enable lib-jitsi-meet logs

--- spot-client/src/common/vendor/jitsi-meet-js-provider.js
+++ spot-client/src/common/vendor/jitsi-meet-js-provider.js
@@ -3,7 +3,17 @@ import JitsiMeetJS from 'lib-jitsi-meet';
 /**
  * Prevent JitsiMeetJS from spamming the console.
  */
-JitsiMeetJS.setLogLevel('error');
+JitsiMeetJS.setLogLevel('trace');
+
+const loggers = {
+    'modules/RTC/TraceablePeerConnection.js': 'info',
+    'modules/statistics/CallStats.js': 'info',
+    'modules/xmpp/strophe.util.js': 'log'
+};
+
+for (const [ id, level ] of Object.entries(loggers)) {
+    JitsiMeetJS.setLogLevelById(level, id);
+}
 
 /**
  * A wrapper around the global JitsiMeetJS, as loaded by lib-jitsi-meet.
