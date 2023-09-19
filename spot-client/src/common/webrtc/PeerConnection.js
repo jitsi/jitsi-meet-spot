b5ee5fb79d9356754fcbe27dd9365fe37a26e9e1
Saúl Ibarra Corretgé <s@saghul.net>
2023-09-19 09:11:05 +0200

    fix(client) use Jitsi's STUN server by default

--- spot-client/src/common/webrtc/PeerConnection.js
+++ spot-client/src/common/webrtc/PeerConnection.js
@@ -3,9 +3,7 @@ import { logger } from 'common/logger';
 
 
 export const DEFAULT_STUN_SERVERS = [
-    { urls: 'stun:stun.l.google.com:19302' },
-    { urls: 'stun:stun1.l.google.com:19302' },
-    { urls: 'stun:stun2.l.google.com:19302' }
+    { urls: 'stun:meet-jit-si-turnrelay.jitsi.net:443' }
 ];
 
 /**
