a0fe709ec558a056f9ec482b594c517deac6c789
Saúl Ibarra Corretgé <s@saghul.net>
2023-11-16 11:58:03 +0100

    feat(electron) drop Bluetooth beacons altogether
    It was only available on macOS and unused on the client side at this
    time.

--- spot-electron/config.js
+++ spot-electron/config.js
@@ -3,17 +3,6 @@ const process = require('process');
 module.exports = {
 
     /**
-     * Config values for the beacon functionality.
-     */
-    beacon: {
-
-        /**
-         * Region ID for the beacon. This is permanent and must be unique for custom deployments.
-         */
-        region: 'bf23c311-24ae-414b-b153-cf097836947f'
-    },
-
-    /**
      * The default URL to connect to.
      */
     defaultSpotURL: process.env.SPOT_URL || 'https://spot.jitsi.net/tv'
