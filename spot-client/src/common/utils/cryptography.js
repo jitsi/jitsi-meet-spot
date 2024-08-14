1a33bb7941a89a0d362b238ae69300c7cff92fbd
Saúl Ibarra Corretgé <s@saghul.net>
2024-08-14 10:29:24 +0200

    feat(client,utils) refactor random char generation
    - Generate actually unique GUIDs
    - Use a better random character generator
    - Use a better alphabet for join codes which eliminates ambiguity

--- spot-client/src/common/utils/cryptography.js
+++ spot-client/src/common/utils/cryptography.js
@@ -1,45 +1,9 @@
 /**
- * Generates a random 8 character long string.
- *
- * @returns {string}
- */
-export function generate8Characters() {
-    const buf = new Uint16Array(2);
-
-    window.crypto.getRandomValues(buf);
-
-    return `${s4(buf[0])}${s4(buf[1])}`;
-}
-
-/**
- * Generate a likely-to-be-unique guid.
+ * Generate a GUID.
  *
  * @private
  * @returns {string} The generated string.
  */
 export function generateGuid() {
-    const buf = new Uint16Array(8);
-
-    window.crypto.getRandomValues(buf);
-
-    return `${s4(buf[0])}${s4(buf[1])}-${s4(buf[2])}-${s4(buf[3])}-${
-        s4(buf[4])}-${s4(buf[5])}${s4(buf[6])}${s4(buf[7])}`;
-}
-
-/**
- * Converts the passed in number to a string and ensure it is at least 4
- * characters in length, prepending 0's as needed.
- *
- * @param {number} num - The number to pad and convert to a string.
- * @private
- * @returns {string} - The number converted to a string.
- */
-function s4(num) {
-    let ret = num.toString(16);
-
-    while (ret.length < 4) {
-        ret = `0${ret}`;
-    }
-
-    return ret;
+    return window.crypto.randomUUID();
 }
