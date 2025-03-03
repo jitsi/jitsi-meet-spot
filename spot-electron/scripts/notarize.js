9d84d3c5a9c87b2b48964bea869a49f5d9b24f93
Saúl Ibarra Corretgé <s@saghul.net>
2025-03-03 22:18:39 +0100

    feat(electron) update notarization script

--- spot-electron/scripts/notarize.js
+++ spot-electron/scripts/notarize.js
@@ -10,7 +10,7 @@ exports.default = async function notarizing(context) {
         return;
     }
 
-    if (!(process.env.NOTARIZE_BUNDLE_ID && process.env.NOTARIZE_APPLE_ID && process.env.NOTARIZE_APPLE_ID_PASSWORD)) {
+    if (!(process.env.NOTARIZE_TEAM_ID && process.env.NOTARIZE_APPLE_ID && process.env.NOTARIZE_APPLE_ID_PASSWORD)) {
         console.info('No parameters found, skipping notarization...');
 
         return;
@@ -19,11 +19,12 @@ exports.default = async function notarizing(context) {
     console.info('Notarizing Mac app...');
 
     const appName = context.packager.appInfo.productFilename;
+    const appPath = `${appOutDir}/${appName}.app`;
 
     return await notarize({
-        appBundleId: process.env.NOTARIZE_BUNDLE_ID,
-        appPath: `${appOutDir}/${appName}.app`,
+        appPath,
         appleId: process.env.NOTARIZE_APPLE_ID,
-        appleIdPassword: process.env.NOTARIZE_APPLE_ID_PASSWORD
+        appleIdPassword: process.env.NOTARIZE_APPLE_ID_PASSWORD,
+        teamId: process.env.NOTARIZE_TEAM_ID
     });
 };
