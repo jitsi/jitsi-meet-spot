ff4e440dbea6f9f8959421aa8850ac037015c6eb
Saúl Ibarra Corretgé <s@saghul.net>
2025-03-11 10:45:28 +0100

    fix(electron) log paths which we are signing

--- spot-electron/scripts/winSign.js
+++ spot-electron/scripts/winSign.js
@@ -8,7 +8,9 @@ exports.default = async function(configuration) {
         console.info('No parameters found, skipping signing...');
         return;
     }
-    if(configuration.path) {
+    console.log(configuration);
+    if (configuration.path) {
+        console.info(`Signing: ${configuration.path}`);
         execSync(`smctl sign --keypair-alias=${process.env.CODE_SIGNER_KEYPAIR_ALIAS} --input "${configuration.path}"`);
     }
 };
